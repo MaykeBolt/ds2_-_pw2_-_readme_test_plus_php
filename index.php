bdcf9cf224db350d10f17603d0e59f6fe724953a
MaykeBolt <mayke.silva6@etec.sp.gov.br>
2023-08-10 20:47:06 -0300

    Adicionado embed e elementos html no php. Tambem testando mudança de cor no README.

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+echo("<h1 style='text-align: center'>Sonic R Musics</h1>");
+echo("<p style='font-size: 20px'>Regal Ruin - Back in Time</p><br>");
+echo("<embed src='https://www.youtube.com/embed/GYzJZkLi7A8' width='560' height='315' type='video/mp4'>")
+?>
+</body>
+</html>
