0c1cd651198ab8018323112d89dae83611d16272
MaykeBolt <mayke.silva6@etec.sp.gov.br>
2023-08-11 06:23:38 -0300

    Adicionado um plano de fundo, e mais outras musicas do Jogo além de outros elementos css.

--- index.php
+++ index.php
@@ -2,12 +2,33 @@
 <html>
 <body>
 <?php
-echo("<h1 style='text-align: center'>Sonic R Musics</h1>");
-echo("<p style='font-size: 20px'>Regal Ruin - Back in Time</p><br>");
-echo("<embed src='https://www.youtube.com/embed/GYzJZkLi7A8' width='560' height='315' type='video/mp4'>");
+echo("<body style='background-image: url(img/radicalcity.png); background-size: 100% 100%; background-attachment: fixed;'>");
+echo("<style> .osttxt {font-size: 20px; padding-left: 2%; color: white; font-family: Geneva} embed {border: 2px white solid;} </style>");
+echo("<h1 style='text-align: center'>{<span style='color: blue'>Musicas do </span><span style='color: yellow'>Sonic</span> <span style='color: red'>R</span>}</h1>");
+
+echo("<p class='osttxt'>Resort Island - Can You Feel The Sunshine?</p><br>");
+echo("<embed src='https://www.youtube.com/embed/XWoekUxnZ7w' width='560' height='315' type='video/mp4' target='_blank'>");
+    echo("<br><br><br>");
+echo ("<p class='osttxt'>Radical City - Living In The City</p><br>");
+echo("<embed src='https://www.youtube.com/embed/VO7OwIhfBjE' width='560' height='315' type='video/mp4' target='_blank'>");
+    echo("<br><br><br>");
+echo("<p class='osttxt'>Regal Ruin - Back in Time</p><br>");
+echo("<embed src='https://www.youtube.com/embed/GYzJZkLi7A8' width='560' height='315' type='video/mp4' target='_blank'>");
+    echo("<br><br><br>");
+echo("<p class='osttxt'>Reactive Factory - Work it Out</p><br>");
+echo("<embed src='https://www.youtube.com/embed/1Z-p1xeq7ao' width='560' height='315' type='video/mp4' target='_blank'>");
+    echo("<br><br><br>");
+echo("<p class='osttxt'>Radiant Emerald - Diamonds In The Sky</p><br>");
+echo("<embed src='https://www.youtube.com/embed/J81_NbpiZb4' width='560' height='315' type='video/mp4' target='_blank'>");
+    echo("<br><br><br>");
+echo("<p class='osttxt'>Super Sonic Racing</p><br>");
+echo("<embed src='https://www.youtube.com/embed/qoNHGngO65U' width='560' height='315' type='video/mp4' target='_blank'>");
+    echo("<br><br><br>");
+echo ("<p class='osttxt'>Number One</p><br>");
+echo("<embed src='https://www.youtube.com/embed/8JmGivtMrDU' width='560' height='315' type='video/mp4' target='_blank'>");
+
 echo("<br><br><br><br><br><br><br><br><br><br><br><br><br>");
-echo("<p style=font-size: 15px>Como trocar de cor no README.md}</p>");
-echo("<img src='https://i.stack.imgur.com/qSkng.png' alt='Como trocar cor no README.md'>");
+echo("<div style='text-align: center'><p style='font-size: 40px; color:white;'>Como trocar de cor no README.md}</p> <img src='https://i.stack.imgur.com/qSkng.png' alt='Como trocar cor no README.md'></div>");
 ?>
 </body>
 </html>
