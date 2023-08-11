ee002aa37fc580ae85fbf7b84e6277b1c655ca7e
MaykeBolt <mayke.silva6@etec.sp.gov.br>
2023-08-10 22:16:09 -0300

    Adicionado instruções de como adicionar cor ao README.md dentro do php.

--- index.php
+++ index.php
@@ -4,7 +4,10 @@
 <?php
 echo("<h1 style='text-align: center'>Sonic R Musics</h1>");
 echo("<p style='font-size: 20px'>Regal Ruin - Back in Time</p><br>");
-echo("<embed src='https://www.youtube.com/embed/GYzJZkLi7A8' width='560' height='315' type='video/mp4'>")
+echo("<embed src='https://www.youtube.com/embed/GYzJZkLi7A8' width='560' height='315' type='video/mp4'>");
+echo("<br><br><br><br><br><br><br><br><br><br><br><br><br>");
+echo("<p style=font-size: 15px>Como trocar de cor no README.md}</p>");
+echo("<img src='https://i.stack.imgur.com/qSkng.png' alt='Como trocar cor no README.md'>");
 ?>
 </body>
 </html>
